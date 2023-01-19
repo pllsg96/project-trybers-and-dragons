a376fb962e7934ad26cefc1635393d3d3df3639c
Pedro Luiz <pedroluizlsg@gmail.com>
2023-01-19 15:27:56 -0300

    feat: req 5 adiciona archetypes para mage, warrior, necromancer e ranger

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,22 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Necromancer extends Archetype {
+  private static _createdArchetypesInstances = 0;
+  private _energyType : EnergyType = 'mana';
+
+  constructor(
+    name: string,
+  ) {
+    super(name);
+    Necromancer._createdArchetypesInstances += 1;
+  }
+
+  public static createdArchetypeInstances(): number {
+    return this._createdArchetypesInstances;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,22 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Ranger extends Archetype {
+  private static _createdArchetypesInstances = 0;
+  private _energyType : EnergyType = 'stamina';
+
+  constructor(
+    name: string,
+  ) {
+    super(name);
+    Ranger._createdArchetypesInstances += 1;
+  }
+
+  public static createdArchetypeInstances(): number {
+    return this._createdArchetypesInstances;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
