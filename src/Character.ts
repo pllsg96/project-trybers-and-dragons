a16e18ebdce1481ceea8bd688fd7ce61efa4c782
Pedro Luiz <pedroluizlsg@gmail.com>
2023-01-20 06:00:22 -0300

    feat: inacabado -  implementa leitura dos atributos e início dos métodos

--- src/Character.ts
+++ src/Character.ts
@@ -1,28 +1,69 @@
-import Archetype from './Archetypes';
+import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import Fighter from './Fighter';
-import Race from './Races';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
 
 export default class Character implements Fighter {
   private _race: Race;
   private _archetype: Archetype;
-  private _maxLifePoints:number ;
+  private _maxLifePoints:number;
   private _lifePoints: number;
   private _strength: number;
   private _defense: number;
   private _dexterity: number;
   private _energy: Energy;
+  private _name: string;
 
   constructor(name: string) {
-    this._race = race;
-    this._dexterity = this.dexterity();
+    this._name = name;
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(this._name, this._dexterity);
+    this._archetype = new Mage(this._name);
+    this._maxLifePoints = (this._race.maxLifePoints / 2);
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
   }
 
-  public get race() {
+  public get race(): Race {
     return this._race;
   }
 
-  private get dexterity(): number {
-    return Math.floor(Math.random() * 10) + 1;
+  public get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get lifePoints(): number {
+    return this.lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  public get defense(): number {
+    return this._defense;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  public get energy(): Energy {
+    return this.energy;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const receivedDamage = this.defense - attackPoints;
+    if (receivedDamage > 0) return receivedDamage;
   }
 }
